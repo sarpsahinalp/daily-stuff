6c3c9f120517c23bc13de1e0f6b6ee58f5e1e989
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-03-28 20:13:11 +0100

    Daily Fix TLE, look at hints for min heap approach instead of duplicates

--- leetcode/2503/solution2503.py
+++ leetcode/2503/solution2503.py
@@ -1,3 +1,6 @@
+from heapq import heappop, heappush
+
+
 class Solution(object):
     def maxPoints(self, grid, queries):
         """
@@ -11,82 +14,39 @@ class Solution(object):
         sort = sorted(indexed_queries, key=lambda x: x[1])
         # Create a graph out of the edges function able to go left, down, right, up
         # Or act as if its a graph create a visited 
-        self.visited = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        self.visited = set()
 
-        out = [0 for _ in range(len(queries))]
-        frontier = []
-        if grid[0][0] < sort[0][1]:
-                frontier.append((0, 0))
+        out = [0] * len(queries)
+        frontier = [(grid[0][0], 0, 0)]
+        self.visited.add((0, 0))
+
         sum = 0
+
         for idx, query in sort:
-            if len(frontier) == 0 and grid[0][0] < query:
-                frontier.append((0, 0))
-            frontier, number = self.bfs(frontier, grid, query)
-            sum += number
+            sum += self.bfs(frontier, grid, query)
             out[idx] = sum
         # Store the visited in a place
         # Initial idea do bfs with visited for tree like search // is it too slow and too many memort
         # Second idea if the first doesn't pass union find would be suitable with some changes
         return out
 
-
-
-    def getPossibleElements(self, grid, i, j, elem, next_run):
-        elems = []
-
-        # Somehow check here if it's only because of the maximum number if so add them to the next numbers frontier instead
-        
-        # Up
-        if i > 0 and not self.visited[i-1][j]:
-            if grid[i-1][j] < elem:
-                elems.append((i-1, j))
-            else:
-                next_run.append((i-1, j))
-
-        # Down
-        if i < len(grid) - 1 and not self.visited[i+1][j]:
-            if grid[i+1][j] < elem:
-                elems.append((i+1, j))
-            else:
-                next_run.append((i+1, j))
-
-        # Left
-        if j > 0 and not self.visited[i][j-1]: 
-            if grid[i][j-1] < elem:
-                elems.append((i, j-1))
-            else:
-                next_run.append((i, j-1))
-
-        # Right
-        if j < len(grid[0]) - 1 and not self.visited[i][j+1]:
-            if grid[i][j+1] < elem:
-                elems.append((i, j+1))
-            else:
-                next_run.append((i, j+1))
-
-        return elems
-
         
     
     def bfs(self, frontier, grid, max):
         element_count = 0
-        next_run = []
-        while len(frontier) != 0:
-            cur = frontier.pop(0)
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
 
-            if self.visited[cur[0]][cur[1]]:
-                continue
+        while frontier and frontier[0][0] < max:
+            val, r, c = heappop(frontier)
 
-            if grid[cur[0]][cur[1]] >= max:
-                next_run.append(cur)
-                continue
-
-            self.visited[cur[0]][cur[1]] = 1
             element_count += 1
 
-            next_elems = self.getPossibleElements(grid, cur[0], cur[1], max, next_run)
-            for el in next_elems:
-                frontier.append(el)
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and (nr, nc) not in self.visited:
+                    self.visited.add((nr, nc))
+                    heappush(frontier, (grid[nr][nc], nr, nc))
         
-        return next_run, element_count
+        return element_count
         
