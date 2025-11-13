652e358a82cd64cdcd8ae0f55691ac9d799ec84c
sarpsahinalp <sarpsahinalp.official@gmail.com>
2025-11-13 12:58:51 +0100

    Optimize code

--- leetcode/3228/sol.cpp
+++ leetcode/3228/sol.cpp
@@ -7,41 +7,24 @@ public:
     int maxOperations(string s) {
         // 1001101
         // 0011101
-        vector<int> ones{};
 
         auto s_length = s.length();
+        uint32_t out{0};
+        uint32_t window{0};
 
         for (size_t i = 0; i < s_length; i++)
         {
-            if (s[i] == '1') {
-                ones.push_back(i);
-            }
-        }
-
-        uint32_t out{0};
-        uint32_t window{1};
-
-        for (size_t i = 0; !ones.empty() && i < ones.size() - 1; i++)
-        {
-            // 0011101
-            auto cur_idx = ones[i];
-            auto second_idx = ones[i + 1];
-            
-            if (second_idx - 1 == cur_idx) {
+            if (s[i] == '0') {
+                while ((i + 1) < s_length && s[i + 1] == '0')
+                {
+                    i++;
+                }
+                out += window;
+            } else {
                 window++;
-                continue;
             }
-
-            // 0, 2
-            out += window;
-
-            window++;
         }
 
-        if (!ones.empty() && s[s_length - 1] != '1') {
-            out += window;
-        }
-        
         return out;
     }
 };
